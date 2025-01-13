821214fcddd7ed7e9d73423e0bb8c825c5156954
Andreas Sommer <andreas.sommer@iwr.uni-heidelberg.de>
2025-01-13 09:46:58 +0100

    Added first files

---
+++ findlines.sh
@@ -0,0 +1,5 @@
+convert $1.pdf -crop 50x100% png:$1
+for f in $1-*; do 
+convert $f -resize 1X1000! -black-threshold 99% -white-threshold 10% -negate -morphology Erode Diamond -morphology Thinning:-1 Skeleton -black-threshold 50% txt:-| \
+sed -e '1d' -e '/black/d' -e 's/^[^,]*,//' -e 's/[(]//g' -e 's/:.*//' -e 's/,/ /g' > $f.txt;
+done
